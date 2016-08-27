5ecde4686b850231168c1654c87d55b69fd943df
liaolongdong <924902324@qq.com>
2016-08-27 23:32:49 +0800

    process_study_finshed!

--- process_demo.js
+++ process_demo.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var zlib = require('zlib');
-
+var http = require('http');
 
 // process对象提供一系列属性，用于但会系统信息。
 // 返回当前进程的命令行参数数组
@@ -73,14 +73,73 @@ var zlib = require('zlib');
 
 // process对象部署了EventEmitter接口，
 // 可以使用on方法监听各种事件，并指定回调函数
-process.on('uncaughtException', function(err){
-	console.log('got an error: %s', err.message);
-	process.exit(1);
-});
-setTimeout(function(){
-	throw new Error('fail!');
-},100);
+// process.on('uncaughtException', function(err){
+// 	console.log('got an error: %s', err.message);
+// 	process.exit(1);
+// });
+// setTimeout(function(){
+// 	throw new Error('fail!');
+// },100);
 
+// SIGINT事件：接收到系统信号SIGINT时触发，
+// 主要是用户按Ctrl + c时触发
+// process.on('SIGINT', function(){
+// 	console.log('Got a SIGINT. CoodBye cruel world!');
+// 	process.exit(0);
+// });
+// console.log('start!');
+// process.on('SIGINT', function(){
+// 	console.log('Ignore Ctrl-C');
+// });
+// console.log('end');
+
+// var server = http.createServer(function(req, res){
+// 	res.writeHead(200, {'Content-type':'text/plain'});
+// 	res.write('Hello World!');
+// 	res.end();
+// }).listen(8888);
+// console.log('Server has started!');
+// process.on('SIGTERM', function(){
+// 	process.exit(0);
+// });
+
+// exit事件在node进程退出时触发
+// process.on('exit', function(){
+// 	console.log('Coodbye');
+// });
+// process.on('exit', function(){
+// 	fs.writeFileSync('./temp/myfile','需要保存到硬盘的信息！');
+// });
+// process.on('exit', function(code){
+// 	console.log('exiting with code:' + code);
+// });
+// 不能执行异步操作
+// process.on('exit', function(code){
+// 	setTimeout(function(){
+// 		console.log('exiting with code:' + code);
+// 	});
+// });
+
+// 没有被捕捉的错误时，触发uncaughtException
+// process.on('uncaughtException', function(err){
+// 	console.error('An uncaught error occurred!');
+// 	console.error(err.stack);
+// 	throw new Error('未捕获错误');
+// });
+// setTimeout(function(){
+// 	throw new Error('occurred error!');
+// },200);
+// setTimeout(function(){
+// 	console.log('本行依然执行！');
+// },500);
+
+// 读取标准输入，这主要是为了不让当前进程退出
+process.stdin.resume();
+process.on('SIGINT', function(){
+	console.log('SIGINT信号，按Control-D退出');
+});
+// 上面代码部署了SIGINT信号的监听函数，
+// 当用户按下Ctrl-C后，会显示提示文字
 
 
 
